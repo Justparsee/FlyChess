3cf707e537d5ca5c7da865dcbe64de16d91093a7
Justparsee <1040769046@qq.com>
2020-12-02 13:15:40 +0800

    init

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    struct Vector2
+    {
+        public int x, y;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static void Cell(int[] args)
+        {
+            //地图(棋盘)
+            //地图单元格的数组
+            //		地图单元格
+            //			位置：
+            //			图标：▲∞★‖■
+            //			类型：地雷，跳跃，抽奖，暂停，方块
+        }
+    }
+}
